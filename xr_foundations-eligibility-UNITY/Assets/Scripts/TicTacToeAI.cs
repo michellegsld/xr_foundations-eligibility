5c70a0376e8210ddbd675bbba81ac9ec7175f593
Michelle Giraldo <michellegsld@gmail.com>
2022-07-15 00:58:39 -0400

    [Update] Makes sure no piece is placed on another by initializing boardState and checking up on it

--- xr_foundations-eligibility-UNITY/Assets/Scripts/TicTacToeAI.cs
+++ xr_foundations-eligibility-UNITY/Assets/Scripts/TicTacToeAI.cs
@@ -15,21 +15,20 @@ public class WinnerEvent : UnityEvent<int>
 
 public class TicTacToeAI : MonoBehaviour
 {
-
 	// to set the ai difficulty, either 0 or 1
 	int _aiLevel;
 
-	///! Used in ???
+	/// Initalized under: StartGame() and Used under: ValidSpot(int x, int y)
 	/// A matrix made up of TicTacToeState choices
 	/// Defines current state of the tic-tac-toe grid
 	TicTacToeState[,] boardState;
 
-	/// ! Used in ???
-	// (easy inspector visual) bool if it's the player's current turn
-	// Use to determine if it's the player's turn
+	/// Used/toggled under: PlayerSelects(int x, int y) and AiSelects(int x, int y)
+	// (easy inspector visual) bool to determine if it's the player's current turn
 	[SerializeField]
 	private bool _isPlayerTurn;
 
+	// Used under: StartGame()
 	// (easy inspector visual) int of current grid size, usually 3 x 3
 	[SerializeField]
 	private int _gridSize = 3;
@@ -45,14 +44,13 @@ public class TicTacToeAI : MonoBehaviour
 	[SerializeField]
 	private GameObject _oPrefab;
 
-	// ! Used in ?? Defined where?
-	// call this event when the game starts under StartGame()
+	// Used under StartGame()
 	public UnityEvent onGameStarted;
 
 	//Call This event with the player number to denote the winner
 	public WinnerEvent onPlayerWin;
 
-	// makes up the squares you fill on the grid
+	// makes up the spots you fill on the grid
 	ClickTrigger[,] _triggers;
 
 	private void Awake()
@@ -77,25 +75,40 @@ public class TicTacToeAI : MonoBehaviour
 	}
 
 	// Called under: StartAI(int AILevel)
-	// Initalizes a new tic-tac-toe grid and invokes the onGameStarted event
+	// Initalizes a new tic-tac-toe grid
 	private void StartGame()
 	{
 		_triggers = new ClickTrigger[3,3];
+
+		// initalizes new boardState to none
+		boardState = new TicTacToeState[_gridSize,_gridSize];
+		for (int i = 0; i < _gridSize; i++) {
+			for (int j = 0; j < _gridSize; j++) {
+				boardState[i,j] = (int) TicTacToeState.none;
+			}
+		}
+
 		onGameStarted.Invoke();
 	}
 
 	// Called under: ClickTrigger.cs -> ClickTrigger.OnMouseDown()
 	// Displays Player icon at a specific point on the tic-tac-toe grid
 	public void PlayerSelects(int coordX, int coordY){
-
-		SetVisual(coordX, coordY, playerState);
+		if (_isPlayerTurn || ValidSpot(coordX, coordY))
+		{
+			SetVisual(coordX, coordY, playerState);
+			_isPlayerTurn = false;
+		}
 	}
 
 	// Called under: ???
 	// Displays AI icon at a specific point on the tic-tac-toe grid
 	public void AiSelects(int coordX, int coordY){
-
-		SetVisual(coordX, coordY, aiState);
+		if (!_isPlayerTurn && ValidSpot(coordX, coordY))
+		{
+			SetVisual(coordX, coordY, aiState);
+			_isPlayerTurn = true;
+		}
 	}
 
 	// Called under: PlayerSelects(int x, int y) and AiSelects(int x, int y)
@@ -108,5 +121,19 @@ public class TicTacToeAI : MonoBehaviour
 			_triggers[coordX, coordY].transform.position,
 			Quaternion.identity
 		);
+
+		boardState[coordX, coordY] = targetState; // Saves this placement
+	}
+
+	private void AiPlacement(){
+		// math to determine placement
+
+	}
+
+	// Call to check if the spot on the grid is free
+	private bool ValidSpot(int x, int y) {
+		if (boardState[x, y] == (int) TicTacToeState.none)
+			return true;
+		return false;
 	}
 }
